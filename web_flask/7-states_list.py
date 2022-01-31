410f21f605fcbaa34c9d9174f5fc292ea671493f
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-01-30 23:44:33 -0500

    functions

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,7 +2,14 @@
 """ Script that starts a Flask web application """
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
