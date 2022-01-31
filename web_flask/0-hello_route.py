dba7973540a0ef1c945af5c53e0afa8c7c3a095d
Julian-Carlosama <67349003+Julian-Carlosama@users.noreply.github.com>
2022-01-30 22:07:24 -0500

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ Script that starts a Flask web application """
-
 from flask import Flask
 app = Flask(__name__)
 
@@ -9,6 +8,7 @@ app = Flask(__name__)
 def hello_hbnb():
     """ Function called with / route """
     return ("Hello HBNB!")
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
